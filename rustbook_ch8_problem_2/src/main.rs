e68ad2ed14db4040bc9b07e5e8ffbeec14f917b2
goglom <goglom12@gmail.com>
2023-06-04 21:26:31 +0700

    initial commit

--- rustbook_ch8_problem_2/src/main.rs
+++ rustbook_ch8_problem_2/src/main.rs
@@ -1,31 +1,36 @@
+use std::io;
 
 fn is_vowel(symbol: &char) -> bool {
     match symbol.to_ascii_lowercase() {
-        'a' |'e'|  'i' | 'o' | 'u' => true,
-        _ => false
+        'a' | 'e' | 'i' | 'o' | 'u' => true,
+        _ => false,
     }
 }
 
 fn main() {
-    let input = "first";
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Something went fron while reading stdin");
+
+    let input = input.trim();
+
     let mut result = String::new();
 
     let mut iter = input.chars();
-    if let Some(fst) = iter.next()  {
+    if let Some(fst) = iter.next() {
         if !fst.is_alphabetic() {
             println!("Expected alphabetic string but hot something else:(");
             return;
         }
 
         if is_vowel(&fst) {
-            result.push_str(input);
+            result.push_str(&input);
             result.push_str("-hay");
-        }
-        else {
+        } else {
             result.push_str(iter.as_str());
             result.push_str(&format!("-{}ay", fst));
         }
-
     }
     println!("Yout pig-latin word: \"{}\"", result);
 }
